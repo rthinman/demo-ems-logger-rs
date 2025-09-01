1293ec3c101ad7f7116e985cd17d3ebb3d3a4c16
Rod Hinman <rod@auroraresearch.com>
2025-08-31 15:44:34 -1000

    Add log_buffer and alarm flags to logger
    Use the arrayvec crate to provide a safe buffer.
    Use the bitflags crate for an efficient way of storing multiple flags.

--- business_logic/src/constants.rs
+++ business_logic/src/constants.rs
@@ -12,6 +12,8 @@ pub const ALARM_LOW_SECONDS: u32 = 60 * 60;       // 1 hour to trigger low temp 
 // Door open times.
 pub const DOOR_ALARM_THRESHOLD: u32 = 300;    // 5 minutes in seconds.
 
-// Aggregation/sampling periods in seconds.
+// Aggregation/sampling.
 pub const SAMPLE_PERIOD: u32 = 15 * 60; // 15 minutes in seconds.
 pub const AGGREGATION_PERIOD: u32 = 8 * 60 * 60; // 8 hours in seconds.
+
+pub const LOG_BUFFER_SIZE: usize = 24; // Number of entries in log buffer (should be enough for 6h at 15min intervals).
--- business_logic/src/logger.rs
+++ business_logic/src/logger.rs
@@ -1,6 +1,9 @@
 //! This module contains the business logic for aggregating temperature, 
 //! door opening, and power data
 
+use arrayvec::ArrayVec;
+use bitflags::bitflags;
+use crate::constants::LOG_BUFFER_SIZE;
 use crate::{aggregator::Aggregator, door, temperatures::Temperatures, timestamp::{Timestamp, TimestampError}};
 
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
@@ -36,10 +39,36 @@ pub enum AlarmTimerExpired {
     Door,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Default)]
+    pub struct AlarmFlags: u8 {
+        const HIGH_TEMP = 0b00000001;
+        const LOW_TEMP  = 0b00000010;
+        const DOOR_OPEN = 0b00000100;
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+struct DataEntry {
+    relt: Timestamp,  // Relative time in seconds since logging started.
+    rtcw: Timestamp,  // RTC value when awoken.
+    tvc: Option<f32>, // Vaccine temperature in degC
+    tamb: Option<f32>, // Ambient temperature in degC
+    // blog: u16, // Logger battery lifetime in days.
+    // cmpr: u16, // Compressor run time this sample in seconds.
+    // cmps: u16, // Compressor maximum speed this sample in RPM.
+    // sva: u16, // Supply voltage availability in seconds this sample.
+    dorv: u16, // Door open time this sample in seconds.
+    dorc: u16, // Door open count this sample.
+    alrm: AlarmFlags, // Bitfield of active alarms
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Logger {
     agg: Aggregator,
     temps: Temperatures, 
+    log_buffer: ArrayVec<DataEntry, LOG_BUFFER_SIZE>,
 }
 
 impl Logger {
@@ -47,10 +76,10 @@ impl Logger {
         Self {
             agg: Aggregator::new(now),
             temps: Temperatures::new(),
+            log_buffer: ArrayVec::new(),
         }
     }
 
-    // TODO: track alarm status so we don't retrigger if already alarming.
     // TODO: do we need to return Result<> here? Where is the best place to protect against out of order timestamps?
     pub fn process_event(&mut self, event: LoggerEvent, ts: Timestamp) -> Result<AlarmTimerTrigger, TimestampError> {
 
@@ -71,9 +100,36 @@ impl Logger {
                     self.agg.cancel_temperature_alarms();
                 }
 
-                // TODO: If samples buffer is full, write to a file. logger.c log_sample()
-                // TODO: Populate an entry in the samples buffer. log_sample()
-                // 
+                // Add sample to buffer
+                let mut alarm_flags = AlarmFlags::empty();
+                if self.temps.is_high_alarm() {
+                    alarm_flags |= AlarmFlags::HIGH_TEMP;
+                }
+                if self.temps.is_low_alarm() {
+                    alarm_flags |= AlarmFlags::LOW_TEMP;
+                }
+
+
+                // TODO: add door alarm check, including new alarm flag clearing below.
+                
+                // Clear new alarm flags.
+                self.temps.clear_new_alarms();
+
+                let entry = DataEntry {
+                    relt: ts, // TODO: convert to relative time
+                    rtcw: ts, // TODO: get actual RTCW
+                    tvc: sample.vaccine,
+                    tamb: sample.ambient,
+                    dorv: 0, // TODO: get from aggregator
+                    dorc: 0, // TODO: get from aggregator
+                    alrm: alarm_flags,
+                };
+                
+                if self.log_buffer.is_full() {
+                    // TODO: write buffer to storage before clearing
+                    self.log_buffer.clear();
+                }
+                self.log_buffer.push(entry);
 
                 (ready, trigger)
             }
